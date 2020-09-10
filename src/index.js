3e2584c615da67194205830154631e22ce707765
Tam Le <tamltn@acb.com.vn>
2020-09-10 10:22:38 +0700

    - upgrade webpack version, remove jquery bootstrap

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import { createStore, applyMiddleware } from 'redux';
-import appReducers from './reducers/index';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import thunk from 'redux-thunk';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'jquery';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import { createStore, applyMiddleware } from "redux";
+import appReducers from "./reducers/index";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import thunk from "redux-thunk";
+import "./styles/styles.scss";
 
 const store = createStore(
-    appReducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(thunk)
+  appReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router><App /></Router>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
